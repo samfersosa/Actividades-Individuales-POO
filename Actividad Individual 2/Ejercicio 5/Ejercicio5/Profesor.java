ed46b1cbf93682ed3e9c970ef70b85e00cdef516
samfersosa <ssosam@unal.edu.co>
2025-05-20 18:39:23 -0500

    ejercicio 5

---
+++ Actividad Individual 2/Ejercicio 5/Ejercicio5/Profesor.java
@@ -0,0 +1,6 @@
+public class Profesor {
+
+    protected void imprimir() {
+    System.out.println("Es un profesor.");
+    }
+    }
---
+++ Actividad Individual 2/Ejercicio 5/Ejercicio5/Prueba.java
@@ -0,0 +1,7 @@
+public class Prueba {
+    
+    public static void main(String[] args) {
+    Profesor profesor1 = new ProfesorTitular();
+    profesor1.imprimir();
+    }
+    }
